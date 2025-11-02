3a93799b8d29ffe32004b01e10ce088e16edace3
Pablo <petxeberria9@gmail.com>
2025-11-02 17:23:17 +0100

    Inicio de proyecto

---
+++ 02_VARIABLES.py
@@ -0,0 +1,18 @@
+# Definicion de variables
+canastas = 7
+canastas_paco = 2
+canastas_lucia = 8
+canastas_tres = 12
+
+ptos_dos = 1
+ptos_dos = 2
+ptos_tres = 3
+
+# Calculos
+total_puntos_paco = canastas_paco * ptos_tres
+
+# Imprimimos los resultados
+print(canastas)
+print(canastas_paco)
+print(canastas_lucia)
+print(total_puntos_paco)
---
+++ 03_TIPOS_VARIABLES.py
@@ -0,0 +1,19 @@
+#Booleanos
+from tokenize import String
+
+verdadero= True
+false = False
+print(verdadero)
+print(false)
+
+#Numero entero o decimal
+edad= 32
+print(edad)
+
+altura= 1.84
+print(altura)
+
+#Texto String
+nombre= "Paco"
+print(nombre )
+
